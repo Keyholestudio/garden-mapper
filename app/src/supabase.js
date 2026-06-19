502e324418349cda91e15ac2d54fe6356ce58ffd
Computer <k3yh0l35tud10@gmail.com>
2026-06-18 20:15:31 -0400

    Auth: revert to implicit flow (PKCE breaks external browser deep-link)

--- app/src/supabase.js
+++ app/src/supabase.js
@@ -5,7 +5,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    flowType: 'pkce',
+    flowType: 'implicit',
     detectSessionInUrl: false,  // We handle URL parsing manually via appUrlOpen
   },
 });
