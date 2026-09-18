f41ff6cb8916ad9d0e8395b88e48824686055862
Computer <k3yh0l35tud10@gmail.com>
2026-09-17 22:23:37 -0400

    shrubs batch 2: 15 approved (9 evergreen + 6 flowering shrubs)

--- app/src/data/packs/pack-shrubs-evergreen.js
+++ app/src/data/packs/pack-shrubs-evergreen.js
@@ -15,4 +15,14 @@ export const entries = [
   { key: 'shrub-evergreen_fatsia_L_CA-US-FR-GB-AU', label: 'Fatsia', size: 'L', latinName: 'Fatsia japonica', searchTerms: ['fatsia', 'japanese aralia', 'castor oil plant'], traits: ['evergreen', 'architectural', 'shade', 'tropical', 'shrub'] },
   { key: 'shrub-evergreen_daphne_S_CA-US-FR-GB-AU', label: 'Daphne', size: 'S', latinName: 'Daphne odora', searchTerms: ['daphne', 'winter daphne', 'fragrant daphne'], traits: ['evergreen', 'fragrant', 'spring', 'shrub'] },
 
+  { key: 'shrub-evergreen_camellia_M_CA-US-FR-GB-AU', label: 'Camellia', size: 'M', latinName: 'Camellia japonica', searchTerms: ['camellia', 'japonica', 'flowering camellia'], traits: ['evergreen', 'flowering', 'ornamental', 'shrub'] },
+  { key: 'shrub-evergreen_elaeagnus_L_CA-US-FR-GB-AU', label: 'Elaeagnus', size: 'L', latinName: 'Elaeagnus x ebbingei', searchTerms: ['elaeagnus', 'oleaster', 'silverberry'], traits: ['evergreen', 'screening', 'fragrant', 'shrub'] },
+  { key: 'shrub-evergreen_aucuba_M_CA-US-FR-GB-AU', label: 'Aucuba', size: 'M', latinName: 'Aucuba japonica', searchTerms: ['aucuba', 'spotted laurel', 'japanese laurel'], traits: ['evergreen', 'shade', 'berries', 'ornamental', 'shrub'] },
+  { key: 'shrub-evergreen_ceanothus_L_CA-US-FR-GB-AU', label: 'Ceanothus', size: 'L', latinName: 'Ceanothus impressus', searchTerms: ['ceanothus', 'california lilac', 'blue blossom'], traits: ['evergreen', 'flowering', 'pollinator', 'drought', 'shrub'] },
+  { key: 'shrub-evergreen_rock-rose_S_CA-US-FR-GB-AU', label: 'Rock Rose', size: 'S', latinName: 'Cistus x purpureus', searchTerms: ['cistus', 'rock rose', 'sun rose'], traits: ['evergreen', 'flowering', 'drought', 'sun', 'shrub'] },
+  { key: 'shrub-evergreen_hebe_S_CA-US-FR-GB-AU', label: 'Hebe', size: 'S', latinName: 'Hebe speciosa', searchTerms: ['hebe', 'shrubby veronica'], traits: ['evergreen', 'flowering', 'coastal', 'compact', 'shrub'] },
+  { key: 'shrub-evergreen_myrtle_M_CA-US-FR-GB-AU', label: 'Myrtle', size: 'M', latinName: 'Myrtus communis', searchTerms: ['myrtle', 'common myrtle', 'myrtus'], traits: ['evergreen', 'fragrant', 'mediterranean', 'shrub'] },
+  { key: 'shrub-evergreen_heather_XS_CA-US-FR-GB-AU', label: 'Heather', size: 'XS', latinName: 'Calluna vulgaris', searchTerms: ['heather', 'ling', 'calluna'], traits: ['evergreen', 'flowering', 'heath', 'acid-soil', 'shrub'] },
+  { key: 'shrub-evergreen_pittosporum_M_CA-US-FR-GB-AU', label: 'Pittosporum', size: 'M', latinName: 'Pittosporum tenuifolium', searchTerms: ['pittosporum', 'kohuhu'], traits: ['evergreen', 'screening', 'fragrant', 'shrub'] },
+
 ];
--- app/src/data/packs/pack-shrubs-flowering.js
+++ app/src/data/packs/pack-shrubs-flowering.js
@@ -8,4 +8,11 @@ export const entries = [
 
   // 🌺 Flowering Shrubs ──────────────────────────────────────────────────────────────
 
+  { key: 'shrub-flowering_choisya_M_CA-US-FR-GB-AU', label: 'Choisya', size: 'M', latinName: 'Choisya ternata', searchTerms: ['choisya', 'mexican orange blossom'], traits: ['evergreen', 'flowering', 'fragrant', 'spring', 'shrub'] },
+  { key: 'shrub-flowering_osmanthus_M_CA-US-FR-GB-AU', label: 'Osmanthus', size: 'M', latinName: 'Osmanthus x burkwoodii', searchTerms: ['osmanthus', 'burkwood osmanthus'], traits: ['evergreen', 'flowering', 'fragrant', 'spring', 'shrub'] },
+  { key: 'shrub-flowering_quince_M_CA-US-FR-GB-AU', label: 'Flowering Quince', size: 'M', latinName: 'Chaenomeles speciosa', searchTerms: ['flowering quince', 'chaenomeles', 'japonica'], traits: ['deciduous', 'flowering', 'spring', 'edible', 'shrub'] },
+  { key: 'shrub-flowering_hydrangea-paniculata_M_CA-US-FR-GB-AU', label: 'Hydrangea Paniculata', size: 'M', latinName: 'Hydrangea paniculata', searchTerms: ['hydrangea paniculata', 'panicle hydrangea', 'limelight'], traits: ['deciduous', 'flowering', 'summer', 'shrub'] },
+  { key: 'shrub-flowering_rose-of-sharon_M_CA-US-FR-GB-AU', label: 'Rose of Sharon', size: 'M', latinName: 'Hibiscus syriacus', searchTerms: ['rose of sharon', 'hibiscus syriacus', 'shrub althaea'], traits: ['deciduous', 'flowering', 'summer', 'long-blooming', 'shrub'] },
+  { key: 'shrub-flowering_russian-sage_M_CA-US-FR-GB-AU', label: 'Russian Sage', size: 'M', latinName: 'Perovskia atriplicifolia', searchTerms: ['russian sage', 'perovskia', 'salvia yangii'], traits: ['deciduous', 'flowering', 'summer', 'drought', 'pollinator', 'shrub'] },
+
 ];
